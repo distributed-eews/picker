fe72d5da12d016ec0ed1c0d76c490e064f027688
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-11-08 13:20:41 +0700

    feat: add time tracker

--- app/processor.py
+++ app/processor.py
@@ -5,6 +5,7 @@ from typing import List, Dict, Any
 from confluent_kafka import Consumer, Producer, KafkaError
 from .missing_data_handler import MissingDataHandler
 import copy
+from datetime import datetime
 
 class KafkaDataProcessor:
     def __init__(self, consumer: Consumer, producer: Producer, data_handler: MissingDataHandler):
@@ -17,7 +18,6 @@ class KafkaDataProcessor:
         i = 1
         while True:
             msg = self.consumer.poll(10.0)
-
             if msg is None:
                 print("No message received")
                 continue
@@ -40,25 +40,25 @@ class KafkaDataProcessor:
                 self._flush(sampling_rate=20)
             if value["type"] == "trace":
                 i+=1
-                self.__process_received_data(value)
+                self.__process_received_data(value, arrive_time=datetime.utcnow())
 
-    def __process_received_data(self, value: Dict[str, Any]):
+    def __process_received_data(self, value: Dict[str, Any], arrive_time: datetime):
         station = value['station']
         channel = value['channel']
+        eews_producer_time = value['eews_producer_time']
         data = value['data']
         start_time = datetime.fromisoformat(value['starttime'])
         sampling_rate = value['sampling_rate']
         if station == "BKB" and channel == "BHE":
             print("Received ", station, channel)
             print("from message: ",value['starttime'])
-            print("from datetime.fromisoformat(): ",start_time)
         self.data_handler.handle_missing_data(
             station, channel, start_time, sampling_rate)
-        if station == "BKB" and channel == "BHE":
-            print("from datetime.fromisoformat(): ",start_time)
-        self.__store_data(station, channel, data, start_time, sampling_rate)
+        self.__store_data(station, channel, data, start_time, sampling_rate, 
+                          eews_producer_time=eews_producer_time, 
+                          arrive_time=arrive_time)
 
-    def __store_data(self, station: str, channel: str, data: List[int], start_time: datetime, sampling_rate: float):
+    def __store_data(self, station: str, channel: str, data: List[int], start_time: datetime, sampling_rate: float, eews_producer_time, arrive_time: datetime):
         if station not in self.data_handler.data_pool:
             self.data_handler.data_pool[station] = {}
         if channel not in self.data_handler.data_pool[station]:
@@ -75,7 +75,9 @@ class KafkaDataProcessor:
             self.data_handler.data_pool[station][channel] = self.data_handler.data_pool[station][channel][128:]
             time_to_add = timedelta(seconds=128/sampling_rate)
             self.__send_data_to_queue(
-                station, channel, data_to_send, current_time, current_time + time_to_add)
+                station, channel, data_to_send, current_time, current_time + time_to_add, 
+                eews_producer_time=eews_producer_time,
+                arrive_time=arrive_time)
             current_time = current_time + time_to_add
 
         remaining_data_len = len(self.data_handler.data_pool[station][channel])
@@ -84,10 +86,12 @@ class KafkaDataProcessor:
         # print(
         #     f"LAST PROCESSED TIME: {self.data_handler.last_processed_time[station][channel]}")
 
-    def __send_data_to_queue(self, station: str, channel: str, data: List[int], start_time: datetime, end_time: datetime):
+    def __send_data_to_queue(self, station: str, channel: str, data: List[int], start_time: datetime, end_time: datetime, eews_producer_time, arrive_time: datetime):
         self.data_handler.update_last_processed_time(
             station, channel, end_time)
-        self.producer.produce(station, channel, data, start_time, end_time)
+        self.producer.produce(station, channel, data, start_time, end_time, 
+                              eews_producer_time=eews_producer_time, 
+                              arrive_time=arrive_time)
     
 
     def _start(self):
--- app/producer.py
+++ app/producer.py
@@ -4,6 +4,7 @@ from confluent_kafka import Producer
 from dotenv import load_dotenv
 import copy
 load_dotenv()
+from datetime import datetime
 
 TOPIC_PRODUCER = os.getenv('TOPIC_PRODUCER')
 
@@ -12,17 +13,21 @@ class KafkaProducer:
     def __init__(self, bootstrap_servers):
         self.producer = Producer({'bootstrap.servers': bootstrap_servers})
 
-    def produce(self, station, channel, data, start_time, end_time):
+    def produce(self, station, channel, data, start_time, end_time, eews_producer_time=[], arrive_time=datetime.utcnow()):
         data = {
             'station': station,
             'channel': channel,
             'starttime': start_time.isoformat(),
             'endtime': end_time.isoformat(),
             'data': data,
-            'len': len(data)
+            'len': len(data),
+            'eews_producer_time': eews_producer_time,
+            'eews_queue_time':[arrive_time.isoformat(), datetime.utcnow().isoformat()]
         }
         if station == "BKB" and channel == "BHE":
             print(("=" * 20) + f"{station}____{channel}" + ("="*20))
-            print(data['starttime'], data['endtime'])
+            print('packet time: ',[data['starttime'], data['endtime']])
+            print('eews_producer_time: ', data['eews_producer_time'])
+            print('eews_queue_time: ', data['eews_queue_time'])
         self.producer.produce(TOPIC_PRODUCER, key=station,
                               value=json.dumps(data))
